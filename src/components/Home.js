8ac11420d7b7814a0ea06123f6ecd32e743ea8cd
Progna_Labony_Roy <labony.progna@gmail.com>
2023-01-15 00:37:29 +0600

    user icon added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,47 @@
-import React, { useContext } from 'react';
-import { useQuery } from 'react-query';
-import { AuthContext } from '../Context/UserContext';
+import React, { useContext } from "react";
+import { useQuery } from "react-query";
+import { AuthContext } from "../Context/UserContext";
 
 const Home = () => {
-    const { user }=useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
-    const url = `http://localhost:5000/users?email=${user?.email}`;
+  const url = `http://localhost:5000/users?email=${user?.email}`;
 
   const { data: users = [] } = useQuery({
     queryKey: ["users", user?.email],
     queryFn: async () => {
-      const res = await fetch(url
-        , {
+      const res = await fetch(url, {
         headers: {
           authorization: `bearer ${localStorage.getItem("accessToken")}`,
         },
-      }
-      );
+      });
       const data = await res.json();
-      console.log(users)
+      console.log(users);
       return data;
     },
   });
 
-    return (
-        <div>
-            <p  className='py-20 font-semibold text-lg'>Welcome <span className='text-indigo-600'>{user?.displayName}</span></p>
-        </div>
-    );
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-20 h-20 mx-auto mt-20"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+        />
+      </svg>
+      <p className="py-3 font-semibold text-xl">
+        Welcome ! <span className="text-indigo-600">{user?.displayName}</span>
+      </p>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -51,6 +51,13 @@ const SignIn = () => {
 
   return (
     <div className="mt-20">
+      <div>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-20 h-20 mx-auto">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+</svg>
+
+      <p  className='py-3 font-semibold text-xl'>Welcome !</p>
+      </div>
       <div className="min-h-full flex items-center justify-center px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full">
           <form onSubmit={handleSubmit}>
